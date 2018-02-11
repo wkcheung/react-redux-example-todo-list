1b0640f739ace81b50d300878f59e9229e8b84c5
wkcheung <wingkeicheung@gmail.com>
2018-02-11 21:49:52 +0800

    Finished Basics

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ import { createStore } from 'redux'
 import todoApp from './src/reducers'
 import App from './src/components/App'
 
+import { VisibilityFilters } from './src/constants/constants'
+import { toggleTodo, setVisibilityFilter, addTodo } from './src/actions'
+
 let store = createStore(todoApp)
+
+// Log the initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
 
 render(
   <Provider store={store}>
